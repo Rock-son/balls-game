8ff2adadfaab1d16efc3a253c407f22ceec1fa06
Rok Zigon <roky.zz@gmail.com>
2020-03-31 22:49:25 +0200

    added build files for production

---
+++ ClientApp/build/precache-manifest.7b8755b82db15c43afddf4945f704d84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3dd930723af2d7bb3e58a915aa1424",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a1d6246645a48b5924",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "c7d5d880b4f0c61ba407",
+    "url": "/static/css/main.9dac136d.chunk.css"
+  },
+  {
+    "revision": "b6a1d6246645a48b5924",
+    "url": "/static/js/2.010687aa.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.010687aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d5d880b4f0c61ba407",
+    "url": "/static/js/main.d51cb944.chunk.js"
+  },
+  {
+    "revision": "bf2a2b4c24cbd6f6363f",
+    "url": "/static/js/runtime-main.2f90d172.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
