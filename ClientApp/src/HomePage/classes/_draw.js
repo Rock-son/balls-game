cab70f481f9f11af631a952f5b75b4f1acce83fa
Rok Zigon <roky.zz@gmail.com>
2020-04-03 01:31:10 +0200

    initial commit

--- ClientApp/src/HomePage/classes/_draw.js
+++ ClientApp/src/HomePage/classes/_draw.js
@@ -1,7 +1,9 @@
-import Particle from "./Particle";
+import { update } from "./update";
+import * as PIXI from "pixi.js";
 import { setDriftlessInterval, clearDriftless } from 'driftless';
+import { text } from "body-parser";
 
-export default function _draw() {	
+export default function _draw() {
 	// UTILITIES
 	function randomIntNumber(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -25,62 +27,78 @@ export default function _draw() {
 		}
 	} = this.state;
 	
-	const canvas = this.canvasRef.current;
-	if (canvas.getContext) {
-		let context = canvas.getContext('2d', { alpha: false });
-		context.mozImageSmoothingEnabled = true;
-		context.webkitImageSmoothingEnabled = true;
-		context.imageSmoothingEnabled = true;
-		context.mozImageSmoothingQuality = "low";
-		context.webkitImageSmoothingQuality = "low";
-		context.imageSmoothingQuality = "low";
-		const particles = [];
-		let color, contagious;
-		const radius = size;
-		const nrParticles = +quantity;
-		const maxWidth = this.canvasWidth - radius * 2.5;
-		const maxHeight = this.canvasHeight - radius * 2.5;
-		for (let i = 0; i < nrParticles; i++) {
-			const mass = 1;
-			color = i === 0 ? "red" : "#aaa";
-			contagious = i === 0 ? 1 : 0;
+	const canvas = this.canvasRef.current;	
+	const app = new PIXI.Application({		
+		backgroundColor: 0x191919,
+		view: canvas,
+		width: this.canvasWidth,
+		height: this.canvasHeight,
+		resolution: window.devicePixelRatio || 1,
+		autoDensity: true
+	});
+	// for many animating objects
+	const container = new PIXI.ParticleContainer();
+	app.stage.addChild(container);
+
+	const loader = PIXI.Loader.shared;
+
+	loader.add("ball-red.png")
+		.add("ball-white.png")
+		.on("progress", (loader, resource) => console.log(loader.progress + "% loaded"))
+		.on("load", (loader, resource) => console.log("Asset loaded" + resource.name))
+		.on("error", err => console.error("load error", err))
+		.on("resize", () => console.warning("this is actualy working? NOT"))
+		.load(handleOnImageLoaded.bind(this));
+
+
+	let color, contagion, img, texture;
+	const images = [];
+	const radius = size;
+	const nrImages = +quantity;
+	const maxWidth = this.canvasWidth - radius * 2.5;
+	const maxHeight = this.canvasHeight - radius * 2.5;
+
+	function handleOnImageLoaded() {
+		const whiteBall = loader.resources["ball-white.png"].texture;
+		const redBall = loader.resources["ball-red.png"].texture;
+		for (let i = 0; i < nrImages; i++) {
+
+			texture = i === 0 ? redBall : whiteBall;
+			contagion = i === 0 ? 1 : 0;
 			let x = randomIntNumber(radius * 2, maxWidth);
 			let y = randomIntNumber(radius * 2, maxHeight);
 			if (i !== 0) {
-				for (let j = 0; j < particles.length; j++) {
-					if ((distance(x, y, particles[j].x, particles[j].y) - (radius + particles[j].radius)) < 0) {
+				for (let j = 0; j < images.length; j++) {
+					if ((distance(x, y, images[j].x, images[j].y) - (radius + images[j].radius)) < 0) {
 						x = randomIntNumber(radius * 2, maxWidth);
 						y = randomIntNumber(radius * 2, maxHeight);
-						// start over
+						// set new x, y recursively
 						j = -1;
 					}
 				}
 			}
-			particles.push(new Particle(context, contagious, x, y, radius, color, speed, mass));			
+			img = new PIXI.Sprite(texture);
+			img.x = x;
+			img.y = y;
+			img.scale.x = .1;
+			img.scale.y = .1;
+			img.anchor.x = .5;
+			img.anchor.y = .5;
+			img.myID = i;
+			img.contagion = contagion;
+			img.radius = radius;
+			img.velocity = { 
+				x: (Math.random() - .5) * speed, 
+				y: (Math.random() - .5) * speed 
+			};
+			images.push(img);
 		}
-
-		// SIMULATION
-		if (this.state.simulation) {
-			this.interval = setDriftlessInterval(
-				() => {
-				const innerWidth = window.innerWidth;
-				const innerHeight = window.innerHeight;
-				const currentCanvasWidth = innerWidth < this.canvasWidth ? this.canvasWidth : innerWidth;
-				const currentCanvasHeight = innerHeight < this.canvasHeight ? this.canvasHeight : innerHeight;
-				if (!this.state.pause && !this.state.stop) {
-					context.clearRect(0,0, currentCanvasWidth, currentCanvasHeight)			
-					particles.forEach(particle => {
-						particle.draw();
-						particle.update(particles, distance, currentCanvasWidth, currentCanvasHeight);
-					});
-				} else if (this.state.pause && !this.state.stop) {
-					return;
-				} else {
-					context.clearRect(0,0, currentCanvasWidth, currentCanvasHeight)
-					context = null;				
-					return clearDriftless(this.interval);
-				}
-			}, 16);
+		const len = images.length;
+		const self = this;
+		// draw and animate
+		for (let index = 0; index < len; index++) {
+			container.addChild(images[index]);
+			app.ticker.add(update.bind(this, images[index], images, distance, loader, self.canvasWidth, self.canvasHeight));
 		}
 	}
 }
