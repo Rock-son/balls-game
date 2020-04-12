425bd6f6a02108ff4acb0c98af6df1931f59f8eb
Rok Zigon <roky.zz@gmail.com>
2020-04-12 02:10:40 +0200

    new build files

---
+++ ClientApp/build/precache-manifest.c374cf00cc35b4b5dc1de91450670f44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f3990da6a83212f94ced75494b3034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2425a6be9d783c40126",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "422f82eccb3114115494",
+    "url": "/static/css/main.f54bfc62.chunk.css"
+  },
+  {
+    "revision": "e2425a6be9d783c40126",
+    "url": "/static/js/2.7b259160.chunk.js"
+  },
+  {
+    "revision": "d8a091fe1bad9795906553b5f7cc5340",
+    "url": "/static/js/2.7b259160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422f82eccb3114115494",
+    "url": "/static/js/main.b042908b.chunk.js"
+  },
+  {
+    "revision": "bf2a2b4c24cbd6f6363f",
+    "url": "/static/js/runtime-main.2f90d172.js"
+  }
+]);
