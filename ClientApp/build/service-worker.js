6d9b50e63ffbfaa8fe93336ff9b09f79a5a6933a
Rok Zigon <roky.zz@gmail.com>
2020-04-14 19:40:00 +0200

    new build files

--- ClientApp/build/service-worker.js
+++ ClientApp/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.808a2ed85e092fcf78af4ed4fabd425a.js"
+  "/precache-manifest.1357880b6a053c81d62048cc2468376f.js"
 );
 
 self.addEventListener('message', (event) => {
