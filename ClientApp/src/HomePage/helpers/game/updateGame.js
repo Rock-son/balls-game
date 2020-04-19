17c1aa9a10df2e07b8d8a5bd15c4862d2d1ba45d
Rok Zigon <roky.zz@gmail.com>
2020-04-20 01:30:36 +0200

    game - calculate collisions only once

--- ClientApp/src/HomePage/helpers/game/updateGame.js
+++ ClientApp/src/HomePage/helpers/game/updateGame.js
@@ -176,22 +176,13 @@ export const updateGame = (sprite, spriteArr, quarantineArr, quarantineObj, circ
 	if ((sprite.y - sprite.radius) < 0) {
 		sprite.velocity.y = -sprite.velocity.y;
 	}
-	/* CALCULATE DEACTIVATION TIME IF APPLIED - MAYBE WILL BE USED FOR PREVOIUS INFECTED - VIOLET
-	if (sprite.reactContext.state.simulationSettings["deactivateAfter"] > 0) {
-		if (sprite.contagiousFrom && (sprite.reactContext.state.currentTime - sprite.contagiousFrom > sprite.reactContext.state.simulationSettings["deactivateAfter"])) {
-			sprite.contagion = 0;
-			sprite.contagiousFrom = 0;
-			sprite.texture = loader.resources.sheet.textures["ball-white.svg"];
-			sprite.reactContext.setState(prevState => ({ contagious: prevState.contagious - 1, healthy: prevState.healthy + 1 }));
-		}
-	}*/
 
 	// CALCULATE COLLISION DETECTION WITH QUARANTINE ONLY WHEN DROPPED - when draggedID changes this eval will be false!
 	if (sprite.myID === sprite.reactContext.state.draggedQuarantine.id && !sprite.reactContext.state.quarantinePlaced) {
 		return;
 	}
 	// CALCULATE COLLISION DETECTION TO ALL OTHER IMAGES - substract Text objects!
-	for (let i = 0; i < spriteArr.length - quarantineObj.length; i++) {
+	for (let i = sprite.myID; i < spriteArr.length - quarantineObj.length; i++) {
 		// don't calculate collisions for same objects
 		if (sprite.myID === spriteArr[i].myID) {
 			continue;
@@ -399,27 +390,4 @@ function distance(x1, y1, x2, y2) {
 	const xDist = x2 - x1;
 	const yDist = y2 - y1;
 	return Math.sqrt(xDist * xDist + yDist * yDist);
-}
-function quarantineCollision(particle, otherParticle) {
-	const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x -particle.x);
-
-	// Store mass in var for better readability in collision equation
-	// const m1 = particle.mass;
-	// const m2 = otherParticle.mass;
-
-	// Velocity before equation
-	const u2 = rotate(otherParticle.velocity, angle);
-
-	// Velocity after 1d collision equation
-	const v2 = { x: 0, y: u2.y };
-
-	// Final velocity after rotating axis back to original location
-	const vFinal2 = rotate(v2, -angle);
-
-	// PRESERVE SPEED - calculate startSpeed and newSpeed ratio and apply it to particle x and y velocities
-	const otherParticlePreservedSpeed = preserveSpeed(otherParticle, vFinal2);
-
-	//
-	otherParticle.velocity.x = -otherParticlePreservedSpeed.x;
-	otherParticle.velocity.y = -otherParticlePreservedSpeed.y;
 }
