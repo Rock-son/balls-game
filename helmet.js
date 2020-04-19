789d400bac3cbfa8ab426668d3150a80318195ec
Rok Zigon <roky.zz@gmail.com>
2020-04-19 15:50:43 +0200

    allowed pep in csp

--- helmet.js
+++ helmet.js
@@ -9,7 +9,7 @@ module.exports = function a(app) {
 	app.use(helmet.contentSecurityPolicy({
 		directives: {
 			"defaultSrc": 	[ "'self'" ],
-			"scriptSrc": 	[ "'self'", "'unsafe-eval'", "https://polyfill.io"],
+			"scriptSrc": 	[ "'self'", "'unsafe-eval'", "https://polyfill.io", "https://code.jquery.com"],
 			"styleSrc": 	[ "'self'", "https://fonts.googleapis.com", "https://cdnjs.cloudflare.com" ],
 			"fontSrc": 		[ "'self'" , "https://fonts.gstatic.com" ],
 			"imgSrc":	    [ "'self'", "data:" ],			
