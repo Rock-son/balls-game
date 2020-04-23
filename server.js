d32c6e0b41e29a7937c557dd06a1be441363c660
Rok Zigon <roky.zz@gmail.com>
2020-04-24 00:41:21 +0200

    fix 5 ip whitelist

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 		console.log("Access IP: ",  ip);
 		return next();
 	}
-	console.log("Rejected Adress: ", ip);
+	console.log("Rejected Adress: ", ip, typeof whitelist, whitelist.indexOf(ip));
 	return res.send("Unauthorized");
 });
 
