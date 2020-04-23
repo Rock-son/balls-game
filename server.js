50298bb10082436c0f01c4da77c96c1ee8553090
Rok Zigon <roky.zz@gmail.com>
2020-04-24 00:27:06 +0200

    fix ips 2nd

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 		console.log("Access IP: ",  ip);
 		return next();
 	}
-	console.log("Rejected Adress: ", ip, whitelist );	
+	console.log("Rejected Adress: ", ip, whitelist.indexOf(ip));
 	return res.send("Unauthorized");
 });
 
